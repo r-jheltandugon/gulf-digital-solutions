e52d2c18f0e447b0b7307ed5d823d76f5616e939
R-jhel B. Tandugon <jherietandugon@gmail.com>
2026-05-08 12:14:19 +0400

    Call NavBar & Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/navbar";
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>Gulf Digital Solutions</h1>
-    </main>
+    <>
+      <Navbar />
+
+      <main className="min-h-screen">
+        <section className="flex h-[80vh] items-center justify-center">
+          <h1 className="text-5xl font-bold">
+            Gulf Digital Solutions
+          </h1>
+        </section>
+      </main>
+
+      <Footer />
+    </>
   );
 }
