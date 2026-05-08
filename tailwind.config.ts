1fd9b5fe04f2e1d9076a36e40350b14be445ff3f
R-jhel B. Tandugon <jherietandugon@gmail.com>
2026-05-08 14:54:15 +0400

    First Commit

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#2563EB",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
